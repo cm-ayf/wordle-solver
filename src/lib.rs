f793531297b7b6466e660e37eac14980ade927c8
CallMe AsYouFeel <cm.ayf2734@gmail.com>
2022-02-04 00:43:04 +0900

    implement cli

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,6 @@ mod word;
 mod status;
 mod word_set;
 
-use word::Word;
-use status::Status;
+pub use word::Word;
+pub use status::Status;
+pub use word_set::WordSet;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::collections::HashSet;
+use wordle_solver::WordSet;
+
 fn main() {
-  println!("Hello, world!");
+  let full_set = HashSet::full();
+  let mut set = full_set.clone();
+
+  let stdin = std::io::stdin();
+
+  for _ in 0..5 {
+    if let Some(word) = set.answer() {
+      println!("{}", word);
+      return;
+    }
+
+    let word = set.suggest(&full_set);
+    println!("{}", word);
+    loop {
+      let mut buf = String::new();
+      match (stdin.read_line(&mut buf), buf[0..5].parse()) {
+        (Ok(_), Ok(status)) => {
+          set.filter(word, &status);
+          break;
+        },
+        _ => eprintln!("error try again"),
+      }
+    }
+  }
+
+  match set.answer() {
+    Some(word) => println!("{}", word),
+    None => eprintln!("failed"),
+  };
 }
