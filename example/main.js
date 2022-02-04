116dc4b6aa0b7ffcd2d2096b6f400ac8efe6e53a
CallMe AsYouFeel <cm.ayf2734@gmail.com>
2022-02-04 21:41:21 +0900

    add example

---
+++ example/main.js
@@ -0,0 +1,23 @@
+const { createInterface } = require('readline');
+const { Solver } = require('../pkg/wordle_solver'); // '@cm-ayf/wordle-solver'
+
+const solver = new Solver();
+const reader = createInterface({
+  input: process.stdin,
+});
+
+console.log(solver.start());
+
+let i = 0;
+
+reader.on("line", (input) => {
+  try {
+    const word = solver.next(input);
+    console.log(word);
+    i++;
+    if (solver.finished() || i >= 5) reader.close();
+  } catch (e) {
+    console.error(e);
+  }
+});
+
