cb43592a9972a734c58957fb9405f9e18adad732
CallMe AsYouFeel <cm.ayf2734@gmail.com>
2022-02-04 15:06:46 +0900

    add crate comment

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,26 @@
+//! # wordle-solver
+//! 
+//! [wordle](https://www.powerlanguage.co.uk/wordle/) solver API.
+//! 
+//! ## usage
+//! 
+//! ```
+//! let mut solver = Solver::new();
+//! 
+//! let word = solver.start()
+//! println!("{}", word); // AIERY
+//! 
+//! let (word, is_answer) = solver.next("__YY_")?;
+//! println!("{}", word); // VROUS
+//! let (word, is_answer) = solver.next("_Y_G_")?;
+//! println!("{}", word); // DURRA
+//! let (word, is_answer) = solver.next("_YY__")?;
+//! println!("{}", word); // FLOCS
+//! let (word, is_answer) = solver.next("_____")?;
+//! println!("{}", word); // REBUT
+//! assert!(is_answer);
+//! ```
+
 mod word;
 mod status;
 mod word_set;
